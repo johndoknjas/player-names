de5861f3e69502c505f51fdf2be48d7f8ac26a39
johndoknjas <jdoknjas@sfu.ca>
2024-02-04 17:57:32 -0800

    Print the number of players, and some extra newlines.

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def extract_player_name(line: str) -> Optional[str]:
 def main():
     path = 'players/' + (sys.argv[1] if len(sys.argv) > 1 else 'players.txt')
     with open(path, 'r', errors='ignore') as file:
-        player_names = [name for line in file if (name := extract_player_name(line))]
-    for name in set(player_names):
+        player_names = set([name for line in file if (name := extract_player_name(line))])
+    print(f'\n{len(player_names)} players:\n')
+    for name in player_names:
         print(name, end=' ')
+    print('\n')
 
 if __name__ == "__main__":
     main()
